7ed006271f9a5ddd7a0d4f285c3e57f2ea903b79
1jss <62594271+1jss@users.noreply.github.com>
2023-04-01 19:44:48 +0200

    Indentation
    Add indentation as 2 spaces instead of tabs
    Indented all source code

--- highlight-md.tcl
+++ highlight-md.tcl
@@ -1,84 +1,84 @@
 # ADDS BOLD TAGS
 proc addMdBoldTags {w} {
-    set cur 1.0
-    while 1 {
-        set cur [$w search -count length -regexp {(\*){2}(\S+)(\*){2}} $cur end]
-        if {$cur eq ""} {break}
-        $w tag add bold $cur "$cur + $length char"
-        $w tag remove variable $cur "$cur + $length char"
-        set cur [$w index "$cur + $length char"]
-    }
-    $w tag configure bold -font {Courier -12 bold}
+  set cur 1.0
+  while 1 {
+    set cur [$w search -count length -regexp {(\*){2}(\S+)(\*){2}} $cur end]
+    if {$cur eq ""} {break}
+    $w tag add bold $cur "$cur + $length char"
+    $w tag remove variable $cur "$cur + $length char"
+    set cur [$w index "$cur + $length char"]
+  }
+  $w tag configure bold -font {Courier -12 bold}
 }
 
 # ADDS ITALIC TAGS
 proc addMdItalicTags {w} {
-    set cur 1.0
-    while 1 {
-        set cur [$w search -count length -regexp {(\*){1}(\S+)(\*){1}} $cur end]
-        if {$cur eq ""} {break}
-        $w tag add italic $cur "$cur + $length char"
-        $w tag remove variable $cur "$cur + $length char"
-        set cur [$w index "$cur + $length char"]
-    }
-    $w tag configure italic -font {Courier -12 italic}
+  set cur 1.0
+  while 1 {
+    set cur [$w search -count length -regexp {(\*){1}(\S+)(\*){1}} $cur end]
+    if {$cur eq ""} {break}
+    $w tag add italic $cur "$cur + $length char"
+    $w tag remove variable $cur "$cur + $length char"
+    set cur [$w index "$cur + $length char"]
+  }
+  $w tag configure italic -font {Courier -12 italic}
 }
 
 # ADDS H1 HEADING TAGS
 proc addMdH1HeadingTags {w} {
-    set cur 1.0
-    while 1 {
-        set cur [$w search -count length -regexp {^\#\ } $cur end]
-        if {$cur eq ""} {break}
-        # Don't comment out if escaped
-        if {[$w get "$cur - 1 char"] ne "\\"} {
-            $w tag add heading1 $cur "$cur lineend"
-            $w tag remove bracket $cur "$cur lineend"
-        }
-        set cur [$w index "$cur lineend"]
+  set cur 1.0
+  while 1 {
+    set cur [$w search -count length -regexp {^\#\ } $cur end]
+    if {$cur eq ""} {break}
+    # Don't comment out if escaped
+    if {[$w get "$cur - 1 char"] ne "\\"} {
+      $w tag add heading1 $cur "$cur lineend"
+      $w tag remove bracket $cur "$cur lineend"
     }
-    $w tag configure heading1 -font {Courier -24 bold}
+    set cur [$w index "$cur lineend"]
+  }
+  $w tag configure heading1 -font {Courier -24 bold}
 }
 
 # ADDS H2 HEADING TAGS
 proc addMdH2HeadingTags {w} {
-    set cur 1.0
-    while 1 {
-        set cur [$w search -count length -regexp {^\#\#\ } $cur end]
-        if {$cur eq ""} {break}
-        # Don't comment out if escaped
-        if {[$w get "$cur - 1 char"] ne "\\"} {
-            $w tag add heading2 $cur "$cur lineend"
-            $w tag remove bracket $cur "$cur lineend"
-        }
-        set cur [$w index "$cur lineend"]
+  set cur 1.0
+  while 1 {
+    set cur [$w search -count length -regexp {^\#\#\ } $cur end]
+    if {$cur eq ""} {break}
+    # Don't comment out if escaped
+    if {[$w get "$cur - 1 char"] ne "\\"} {
+      $w tag add heading2 $cur "$cur lineend"
+      $w tag remove bracket $cur "$cur lineend"
     }
-    $w tag configure heading2 -font {Courier -20 bold}
+    set cur [$w index "$cur lineend"]
+  }
+  $w tag configure heading2 -font {Courier -20 bold}
 }
 # ADDS H3 HEADING TAGS
 proc addMdH3HeadingTags {w} {
-    set cur 1.0
-    while 1 {
-        set cur [$w search -count length -regexp {^\#\#\#\ } $cur end]
-        if {$cur eq ""} {break}
-        # Don't comment out if escaped
-        if {[$w get "$cur - 1 char"] ne "\\"} {
-            $w tag add heading3 $cur "$cur lineend"
-            $w tag remove bracket $cur "$cur lineend"
-        }
-        set cur [$w index "$cur lineend"]
+  set cur 1.0
+  while 1 {
+    set cur [$w search -count length -regexp {^\#\#\#\ } $cur end]
+    if {$cur eq ""} {break}
+    # Don't comment out if escaped
+    if {[$w get "$cur - 1 char"] ne "\\"} {
+      $w tag add heading3 $cur "$cur lineend"
+      $w tag remove bracket $cur "$cur lineend"
     }
-    $w tag configure heading3 -font {Courier -16 bold}
+    set cur [$w index "$cur lineend"]
+  }
+  $w tag configure heading3 -font {Courier -16 bold}
 }
 proc highlightMd {handle} {
-    $handle tag remove heading1 0.0 end
-    $handle tag remove heading2 0.0 end
-    $handle tag remove heading3 0.0 end
-    addMdH1HeadingTags $handle
-    addMdH2HeadingTags $handle
-    addMdH3HeadingTags $handle
-    addMdItalicTags $handle
-    addMdBoldTags $handle
+  $handle tag remove heading1 0.0 end
+  $handle tag remove heading2 0.0 end
+  $handle tag remove heading3 0.0 end
+  addMdH1HeadingTags $handle
+  addMdH2HeadingTags $handle
+  addMdH3HeadingTags $handle
+  addMdItalicTags $handle
+  addMdBoldTags $handle
 }
 
 
--- main.tcl
+++ main.tcl
@@ -21,73 +21,76 @@ set activeFileType ""
 
 # SELECT AND APPLY HIGHLIGHT
 proc highlight {} {
-    global activeFileType
-    if {$activeFileType==".tcl"} {
-        highlightTcl .textBoxHandle
-    } elseif {$activeFileType==".md"} {
-        highlightMd .textBoxHandle
-    }
+  global activeFileType
+  if {$activeFileType==".tcl"} {
+    highlightTcl .textBoxHandle
+  } elseif {$activeFileType==".md"} {
+    highlightMd .textBoxHandle
+  }
 }
 
 # NORMAL INPUT
 proc newTextInput { inputId } {
-    return [text .$inputId -font {Helvetica -12} -background gray15 -foreground gray50 -borderwidth 0 -highlightthickness 1 -highlightcolor gray30 -highlightbackground gray20 -selectborderwidth 0 -selectbackground turquoise -selectforeground turquoise4 -insertbackground gray50 -insertwidth 1 -insertofftime 500 -insertontime 500 -padx 5 -pady 5]
+  return [text .$inputId -font {Helvetica -12} -background gray15 -foreground gray50 -borderwidth 0 -highlightthickness 1 -highlightcolor gray30 -highlightbackground gray20 -selectborderwidth 0 -selectbackground turquoise -selectforeground turquoise4 -insertbackground gray50 -insertwidth 1 -insertofftime 500 -insertontime 500 -padx 5 -pady 5]
 }
 
 # SIDEBAR MENU ITEM
 proc newMenuItem { itemId itemText } {
-    return [label .$itemId -font {Helvetica -12} -text $itemText -background gray0 -foreground gray50 -borderwidth 0 -highlightthickness 0 -activebackground gray2 -activeforeground gray60 -anchor w -padx 10]
+  return [label .$itemId -font {Helvetica -12} -text $itemText -background gray0 -foreground gray50 -borderwidth 0 -highlightthickness 0 -activebackground gray2 -activeforeground gray60 -anchor w -padx 10]
 }
 
 # TEXT BOX
 proc newTextBox { inputId } {
-    return [text .$inputId -font {Courier -12} -background gray10 -foreground gray70 -borderwidth 0 -highlightthickness 0 -selectbackground DarkSlateGray -selectforeground gray80 -insertbackground gray50 -insertwidth 1 -insertofftime 500 -insertontime 500 -padx 15 -pady 15 -undo true -autoseparators true -wrap word ]
+  return [text .$inputId -font {Courier -12} -background gray10 -foreground gray70 -borderwidth 0 -highlightthickness 0 -selectbackground DarkSlateGray -selectforeground gray80 -insertbackground gray50 -insertwidth 1 -insertofftime 500 -insertontime 500 -padx 15 -pady 15 -undo true -autoseparators true -wrap word ]
 }
 
 proc openFile { origin } {
-    global activeFile
-    global activeFileType
-    set activeFile $origin
-    set activeFileType [file extension $activeFile]
-    set fileReader [open $activeFile r]
-    .textBoxHandle delete 0.0 end
-    .textBoxHandle insert 0.0 [read $fileReader]
-    close $fileReader
-    
-    highlight
+  global activeFile
+  global activeFileType
+  set activeFile $origin
+  set activeFileType [file extension $activeFile]
+  set fileReader [open $activeFile r]
+  .textBoxHandle delete 0.0 end
+  .textBoxHandle insert 0.0 [read $fileReader]
+  close $fileReader
+  highlight
 }
 
 proc saveFile {} {
-    global activeFile
-    if {$activeFile == ""} {
-        return
-    } else {
-        set fileWriter [ open $activeFile w ]
-        puts -nonewline $fileWriter [.textBoxHandle get 0.0 end]
-        close $fileWriter
-    }
-    highlight
+  global activeFile
+  if {$activeFile == ""} {
+    return
+  } else {
+    set fileWriter [ open $activeFile w ]
+    puts -nonewline $fileWriter [.textBoxHandle get 0.0 "end - 1 char"]
+    close $fileWriter
+  }
+  highlight
 }
 
 # FILLS SIDEBAR FILE MENU
 proc fillSidebarFileMenu {} {
-    # Sidebar Y position iterator to place widgets at
-    set sbY 46
-    set files [glob *]
-    set fileId 0
-    foreach file $files { 
-        destroy .$fileId
-        set .fileId [newMenuItem $fileId $file]
-        bind .$fileId <ButtonPress-1> [list openFile $file] 
-        place .$fileId -in .sidebar -x 0 -y $sbY -width 160 -height 26
-        incr sbY 26
-        incr fileId
-    }
-    # Empty the rest of the list
-    while {$fileId < 100} {
-        destroy .$fileId
-        incr fileId
-    }
+  # Sidebar Y position iterator to place widgets at
+  set sbY 46
+  set files [glob *]
+  set fileId 0
+  foreach file $files { 
+    destroy .$fileId
+    set .fileId [newMenuItem $fileId $file]
+    bind .$fileId <ButtonPress-1> [list openFile $file] 
+    place .$fileId -in .sidebar -x 0 -y $sbY -width 160 -height 26
+    incr sbY 26
+    incr fileId
+  }
+  # Empty the rest of the list
+  while {$fileId < 100} {
+    destroy .$fileId
+    incr fileId
+  }
+}
+
+proc indentRow {} {
+  .textBoxHandle insert insert "  "
 }
 
 # SEARCH INPUT
@@ -98,14 +101,13 @@ place .searchInputHandle -in .sidebar -x 10 -y 10 -width 140 -height 26
 newTextBox "textBoxHandle"
 place .textBoxHandle -in .body -relwidth 1.0 -relheight 1.0
 
-
 # OPEN FILE OR DIR FROM CMD ARGUMENT
 set argument [lindex $argv 0]
 if { [file isdirectory $argument] } {
-    cd $argument
+  cd $argument
 } elseif { [file isfile $argument] } {
-    cd [file dirname $argument]
-    openFile [file tail $argument]
+  cd [file dirname $argument]
+  openFile [file tail $argument]
 }
 
 # EVENT LISTENERS
@@ -115,16 +117,10 @@ bind . <<Save>> {saveFile}
 event add <<Refresh>> <Control-r>
 event add <<Refresh>> <Command-r>
 bind . <<Refresh>> {fillSidebarFileMenu}
+event add <<Indent>> <Tab>
+bind .textBoxHandle <<Indent>> {[indentRow] [break]}
 
 # FILE LIST
 fillSidebarFileMenu
-
-
-
-
-
-
-
-
 
 
